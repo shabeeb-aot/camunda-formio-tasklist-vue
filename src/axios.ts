20a86d445babca6399e2910df1307527c486ede2
kurianbenoy <kurian.benoy@aot-technologies.com>
2021-02-22 13:51:10 +0530

    update README and axios file

--- src/axios.ts
+++ src/axios.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-export function bpmAxios () {
+export function bpmAxios (bearerToken) {
     
   return (
     axios.create({
-      baseURL: 'http://localhost:8080/engine-rest',
+      baseURL: 'http://192.168.0.16:8000/camunda/engine-rest/',
       withCredentials: false,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${bearerToken}`
+      }
     })
   )
 }  
