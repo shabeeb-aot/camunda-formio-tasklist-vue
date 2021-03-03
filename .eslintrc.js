4d59b2b496ab9a55cb302b5de32528e3f6af4bc7
kurianbenoy <kurian.benoy@aot-technologies.com>
2021-03-03 11:20:27 +0530

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,17 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vue/no-unused-components": "warn",
+    "sort-imports": "warn",
+    camelcase: "warn",
+    "vue/valid-template-root": "warn",
+    "indent": ["warn", 4],
+    "eqeqeq": "warn",
+    "handle-callback-err": "warn",
+    "prefer-promise-reject-errors": "warn",
+    "no-unused-vars": "warn",
+    "no-undef": "warn",
+    "new-cap": "warn",
   }
 }
