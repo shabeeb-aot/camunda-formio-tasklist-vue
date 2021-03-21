ca98f18c6454f5a552aa4c49b84904c3dea5dbb5
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2021-03-20 20:48:15 -0700

    build update

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,25 @@
-import TaskList from "./tasklist.vue";
-import FormList from "./formlist.vue";
+// import TaskList from "./tasklist.vue";
+// import FormList from "./formlist.vue";
 
-// export components for this library
-export default {
-    TaskList,
-    FormList,
-};
+// // export components for this library
+// export default {
+//     TaskList,
+//     FormList,
+// };
 
 // export default Components;
+
+import Vue from 'vue'
+
+// https://github.com/talk-to/vue-components
+const vueComponents = require.context('.', false, /[\w-]+.vue$/)
+
+vueComponents.keys().forEach((filename) => {
+  // Get Component Config
+  const componentConfig = vueComponents(filename)
+
+  // Get PascalCase name of component
+  const componentName = filename.replace(/^\.\//, '').replace(/\.\w+$/, '')
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
