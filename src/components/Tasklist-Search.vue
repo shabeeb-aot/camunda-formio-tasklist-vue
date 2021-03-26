d8a342084879c85b096c258c0e75c345a0ceb202
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-03-26 16:11:43 +0530

    create Tasklist search component

---
+++ src/components/Tasklist-Search.vue
@@ -0,0 +1,175 @@
+<template>
+    <div class="cft-input-filter">
+          <b-col class="cft-filter-container" cols="*" xl="12" lg="12" md="12" sm="12">
+            <div class="cft-search-criteria" v-if="searchList.length">
+              <b-button
+                squared
+                variant="outline-secondary"
+                @click="searchAllCriteria"
+              >
+                {{searchA}}
+              </b-button>
+                <span class="cft-search-item-criteria"> of the criteria are met.</span>
+            </div>
+            <div v-if="searchList&&searchList.length">
+              <div class="cftf-search-item-box mr-2"
+                v-for="(item, index) in searchList"
+                :key="item.label"  
+              >
+              <span @click="deleteSearchListElement(index)"><i class="bi bi-x"></i></span>
+                <span title="type" @click="showUpdateSearchList">{{item.label}}</span>
+
+                <div
+                v-if="showUpdatesearch"
+                class="cft-sort-items"
+              >
+                <div
+                  v-for="s in searchListElements"
+                  :key="s.label"
+                  @click="updateSearchListElement(s, index)"
+                  class="mb-2 cft-sort-element"
+                >
+                  {{ s.label }}
+                </div>
+              </div>
+
+                <br>
+                <p title="operator" @click="showOperatorList">
+                  {{operator[index]}}
+                </p>
+                <div v-if="showOperators&&item.compares.length>1">
+                  <div v-for="x in item.compares" :key="x">
+                    <span @click="updateOperators(x, index)">{{x}}</span>
+                  </div>
+                </div>
+                <input
+                  v-model="searchItem[index]"
+                  v-on:keyup.enter="callSearchApi(searchItem[index], item, operator)"/>
+                <span @click="callSearchApi(searchItem[index], item, operator)">
+                  <i class="bi bi-check"></i>
+                </span>
+                <i class="bi bi-x"></i>
+              </div>            
+            </div>
+					<input type="text" class="cft-filter" placeholder="Filter Tasks"
+          @click="cftshowSearchListElements"/>
+            {{tasklength}}
+          <b-list-group
+            v-if="showSearchList"
+            class="cft-search-items"
+          >
+            <b-list-group-item button
+              v-for="(s, idx) in searchListElements"
+              :key="s.label"
+              @click="addSearchElementItem(s, index);setActiveSearchItem(idx)"
+              :class="{'cft-search-item-selected': index ==activeSearchItem }"
+            >
+            {{s.label}}
+            </b-list-group-item>
+          </b-list-group>
+        </b-col>
+      </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import {
+  searchData,
+  searchQuery,
+  sortingList
+} from "../services/utils";
+
+@Component
+export default class TaskListSearch extends Vue{
+
+  @Prop({}) private token !: any;
+  @Prop() private bpmApiUrl !: string;
+
+  private activeSearchItem = 0;
+  private searchListElements: any = searchData;
+  private searchA = 'ALL';
+  private showSearchList = false;
+  private searchList: any = [];
+  private showUpdatesearch = false;
+  private setshowUpdatesearch = false;
+  private showOperators = false;
+  private searchItem = []
+  private operator: any = []
+
+  searchAllCriteria() {
+    if(this.searchA === "ALL") {
+      this.searchA = "ANY"
+    }
+    else {
+      this.searchA = "ALL"
+    }
+  }
+
+  addSearchElementItem(item: any, index: number) {
+    this.searchList.push(item);
+    if(this.searchList===[]){
+      this.operator[0] = item["compares"][0]
+    }
+    else{
+      console.log(this.searchList.length)
+      console.log("item", item["compares"][0])
+      this.operator[this.searchList.length-1] = item["compares"][0]
+    }
+    this.showSearchList = false;
+  }
+
+  deleteSearchListElement(index: any) {
+    this.searchList.splice(index, 1);
+    this.operator.splice(index, 1);
+  }
+
+  showUpdateSearchList() {
+    this.showUpdatesearch = !this.showUpdatesearch;
+  }
+
+  updateSearchListElement(searchitem: any, index: number) {
+    this.searchList[index].label = searchitem.label;
+    this.searchList[index].compares = searchitem.compares;
+    this.searchList[index].values = searchitem.values;
+    this.operator[index] = searchitem.compares[0];
+
+    // this.showsearchIndex = index;
+    this.showUpdatesearch = false;
+  }
+
+  // callSearchApi(item: any, searchItem: any, comparator: string) {
+  //   console.log("Value to search", searchItem["values"])
+  //   console.log("Search value", item);
+  //   console.log("comparator", comparator);
+  //   let index = 0;
+  //   for(let i=0; i<searchItem["compares"].length; i++) {
+  //     if(searchItem["compares"][i]===comparator){
+  //       index = i;
+  //       break;
+  //     }
+  //   }
+  //   console.log("index", searchItem["compares"][index]);
+  //   if(searchItem["compares"][index]==="like") {
+  //     searchQuery[0][searchItem["values"][index]] = '%' + item+ '%';
+  //   }
+
+  //   else{
+  //     searchQuery[0][searchItem["values"][index]] = item;
+  //   }
+
+  //   this.payload["orQueries"] = searchQuery;
+  //   console.log(this.payload)
+  //   this.fetchTaskList(this.selectedfilterId, this.payload);
+  // }
+
+  showOperatorList() {
+    this.showOperators = !this.showOperators;
+  }
+
+  updateOperators(operator: any, index: number) {
+    this.operator[index] = operator;
+
+    this.showOperators = false;
+  }
+}
+</script>
