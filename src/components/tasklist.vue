2dfa07d8ae9dd4d22bd192579956b41dcfedc078
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-03-29 16:36:47 +0530

    remove tasklist sorting component code

--- src/components/tasklist.vue
+++ src/components/tasklist.vue
@@ -22,10 +22,7 @@
           </b-list-group-item>
         </b-list-group>
       </div>
-      <!-- Forms list & fill section -->
-        <FormList :token="token" :bpmApiUrl="bpmApiUrl"/>
-
-    <!-- <b-col cols="*" xl="3" lg="3" md="3" sm="12" class="cft-first"> -->
+      <FormListModal :token="token" :bpmApiUrl="bpmApiUrl"/>          
       <div class="cft-first">
         <!-- Sorting section -->
 				<div id="cftf-dpdown-container" class="mx-2">
@@ -71,108 +68,10 @@
           </TaskSortOptions>
           </div>
 				</div>
-    <!-- </b-col> -->
   </div>
 	<b-row class="cft-service-task-list mt-1">
 		<b-col cols="*" xl="3" lg="3" md="3" sm="12" class="cft-first">
-      <!-- Search Section <TaskListSearch/> -->
-        <div class="cft-input-filter">
-          <b-col class="cft-filter-container" cols="*" xl="12" lg="12" md="12" sm="12">
-            <div class="cft-search-criteria" v-if="searchList.length">
-              <b-button
-                squared
-                variant="outline-secondary"
-                @click="searchAllCriteria"
-              >
-                {{ searchA }}
-              </b-button>
-              <span class="cft-search-item-criteria">
-                of the criteria are met.</span
-              >
-            </div>
-            <div v-if="searchList && searchList.length">
-              <div
-                class="cftf-search-item-box mr-2"
-                v-for="(item, index) in searchList"
-                :key="item.label"
-              >
-                <span @click="deleteSearchListElement(index)"
-                  ><i class="bi bi-x"></i
-                ></span>
-                <span title="type" @click="showUpdateSearchList">{{
-                  item.label
-                }}</span>
-
-                <div v-if="showUpdatesearch" class="cft-sort-items">
-                  <div
-                    v-for="s in searchListElements"
-                    :key="s.label"
-                    @click="updateSearchListElement(s, index)"
-                    class="mb-2 cft-sort-element"
-                  >
-                    {{ s.label }}
-                  </div>
-                </div>
-
-                <br />
-                <p title="operator" @click="showOperatorList">
-                  {{ operator[index] }}
-                </p>
-                <div v-if="showOperators && item.compares.length > 1">
-                  <div v-for="x in item.compares" :key="x">
-                    <span @click="updateOperators(x, index)">{{ x }}</span>
-                  </div>
-                </div>
-                <div v-if='item.type==="date"'>
-                  <!-- <span v-if="showSearchs[index]==='a'" @click="updatesearchinput(index)">
-                  ??</span>                   v-if="showSearchs[index]==='i'"-->
-                  <DatePicker
-                  type="datetime"
-                  v-model="searchDate[index]"
-                  @change="callSearchDateApi(searchDate[index], item, operator, index)"
-                ></DatePicker>
-                </div>
-                <div v-else-if='item.type==="variables"'>
-                  Task variables
-                </div>
-                <div v-else>
-                <span v-if="showSearchs[index]==='a'" @click="updatesearchinput(index)">
-                  ??</span>
-                <input
-                  v-if="showSearchs[index]==='i'"
-                  v-model="searchItem[index]"
-                  v-on:keyup.enter="
-                    callSearchApi(searchItem[index], item, operator, index)
-                  "
-                />
-                <span v-if="showSearchs[index]==='s'" @click="updateresultOnclick(index)">
-                  {{searchItem[index]}}
-                </span>
-                <span @click="callSearchApi(searchItem[index], item, operator, index)">
-                  <i class="bi bi-check"></i>
-                </span>
-                <i class="bi bi-x"></i>
-                </div>
-              </div>
-            </div>
-          <input type="text" class="cft-filter" placeholder="Filter Tasks"
-          @click="cftshowSearchListElements"/>
-            {{tasklength}}
-          <b-list-group
-            v-if="showSearchList"
-            class="cft-search-items"
-          >
-            <b-list-group-item button
-              v-for="(s, idx) in searchListElements"
-              :key="s.label"
-              @click="addSearchElementItem(s);setActiveSearchItem(idx)"
-              :class="{'cft-search-item-selected':idx ==activeSearchItem }"
-            >
-            {{s.label}}
-            </b-list-group-item>
-          </b-list-group>
-        </b-col>
-      </div>
+      <TaskListSearch :tasklength="tasklength"/>
         <!-- Task list section -->
         <b-list-group class="cft-list-container" v-if="tasks && tasks.length">
           <b-list-group-item
@@ -338,7 +237,6 @@
                   data-title="Reset assignee"
                 >
                   <i class="bi bi-person-fill cft-person-fill" />
-                  <!--   <span>{{ task.assignee }}</span>-->
                   <span class="cft-user-span">
                   <b-form-select
                     class="cft-user-select"
@@ -413,9 +311,9 @@
 
 <script lang="ts">
 // removed for this project since its making some issue
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-// import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import "font-awesome/scss/font-awesome.scss";
 import "formiojs/dist/formio.full.min.css";
 import "vue2-datepicker/index.css";
@@ -436,7 +334,7 @@ import BpmnJS from "bpmn-js";
 import CamundaRest from "../services/camunda-rest";
 import DatePicker from "vue2-datepicker";
 import { Form } from "vue-formio";
-import FormList from "../components/formlist.vue";
+import FormListModal from "./FormListModal.vue";
 import Modeler from "bpmn-js/lib/Modeler";
 import TaskListSearch from "../components/Tasklist-Search.vue";
 import TaskSortOptions from "../components/tasklist-sortoptions.vue";
@@ -449,7 +347,7 @@ import vueBpmn from "vue-bpmn";
   components: {
     formio: Form,
     DatePicker,
-    FormList,
+    FormListModal,
     TaskListSearch,
     TaskSortOptions,
     vueBpmn,
@@ -506,31 +404,17 @@ export default class Tasklist extends Vue {
   private selectedfilterId = "";
   private xmlData: any;
   private sortList = TASK_FILTER_LIST_DEFAULT_PARAM;
-  private sortOptions: any = [];
-  private userList: any = [];
-  private updateSortOptions: any = [];
-  private setSortListDropdown = false;
-  private setSortListDropdownindex: any = null;
+  private sortOptions: Array<object> = [];
+  private userList: Array<object> = [];
+  private updateSortOptions: Array<object> = [];
+  private setupdateSortListDropdownindex = 0;
   private showSortListDropdown = [false, false, false, false, false, false];
   private showaddNewSortListDropdown = false;
-  private optionsList: any = ["created"];
-  private payload: any = {
-    active: true,
-    sorting: TASK_FILTER_LIST_DEFAULT_PARAM,
+  private payload: object = {
+    "active": true,
+    "sorting": TASK_FILTER_LIST_DEFAULT_PARAM,
   };
-  private activeSearchItem = 0;
-  private searchListElements: any = searchData;
-  private searchA = "ANY";
-  private showSearchList = false;
   private showUserList = false;
-  private searchList: any = [];
-  private showUpdatesearch = false;
-  private setshowUpdatesearch = false;
-  private showOperators = false;
-  private searchItem = [];
-  private operator: any = [];
-  private showSearchs: any = [];
-  private searchDate: any = [];
 
   checkPropsIsPassedAndSetValue() {
     if (!this.bpmApiUrl || this.bpmApiUrl === "") {
@@ -592,114 +476,9 @@ export default class Tasklist extends Vue {
     this.activefilter = index;
   }
 
-  setActiveSearchItem(index: number) {
-    this.activeSearchItem = index;
-  }
-
-  cftshowSearchListElements() {
-    this.showSearchList = !this.showSearchList;
-  }
   cftShowUserList() {
     this.showUserList = !this.showUserList;
-  //this.use = !this.showSearchList;
   }
-  searchAllCriteria() {
-    if (this.searchA === "ALL") {
-      this.searchA = "ANY";
-    } else {
-      this.searchA = "ALL";
-    }
-  }
-
-  addSearchElementItem(item: any) {
-    this.searchList.push(item);
-    if (this.searchList === []) {
-      this.operator[0] = item["compares"][0];
-      this.showSearchs[0] = 'a';
-    } else {
-      this.operator[this.searchList.length - 1] = item["compares"][0];
-      this.showSearchs[this.searchList.length - 1] = 'a';
-    }
-    this.showSearchList = false;
-  }
-
-  deleteSearchListElement(index: any) {
-    this.searchList.splice(index, 1);
-    this.operator.splice(index, 1);
-  }
-
-  showUpdateSearchList() {
-    this.showUpdatesearch = !this.showUpdatesearch;
-  }
-
-  updateSearchListElement(searchitem: any, index: number) {
-    this.searchList[index].label = searchitem.label;
-    this.searchList[index].compares = searchitem.compares;
-    this.searchList[index].values = searchitem.values;
-    this.operator[index] = searchitem.compares[0];
-
-    this.showUpdatesearch = false;
-  }
-
-  callSearchApi(item: any, searchItem: any, comparator: string, idx: number) {
-    this.showSearchs[idx] = 's'
-    let index = 0;
-    for (let i = 0; i < searchItem["compares"].length; i++) {
-      if (searchItem["compares"][i] === comparator) {
-        index = i;
-        break;
-      }
-    }
-    if (searchItem["compares"][index] === "like") {
-      searchQuery[0][searchItem["values"][index]] = "%" + item + "%";
-    } else {
-      searchQuery[0][searchItem["values"][index]] = item;
-    }
-
-    this.payload["orQueries"] = searchQuery;
-    this.fetchTaskList(this.selectedfilterId, this.payload);
-  }
-
-  callSearchDateApi(item: any, searchItem: any, comparator: string, idx: number) {
-    this.showSearchs[idx] = 's'
-    let index = 0;
-    for (let i = 0; i < searchItem["compares"].length; i++) {
-      if (searchItem["compares"][i] === comparator) {
-        index = i;
-        break;
-      }
-    }
-
-    const timearr = moment(item)
-      .format("yyyy-MM-DD[T]HH:mm:ss.SSSZ")
-      .split("+");
-    const replaceTimezone = timearr[1].replace(":", "");
-    const titem = moment(item).format("yyyy-MM-DD[T]HH:mm:ss.SSSZ")
-      .replace(timearr[1], replaceTimezone);
-
-    searchQuery[0][searchItem["values"][index]] = titem;
-    this.payload["orQueries"] = searchQuery;
-    this.fetchTaskList(this.selectedfilterId, this.payload);
-  }
-
-  showOperatorList() {
-    this.showOperators = !this.showOperators;
-  }
-
-  updateOperators(operator: any, index: number) {
-    this.operator[index] = operator;
-
-    this.showOperators = false;
-  }
-
-  updatesearchinput(index: number) {
-    this.showSearchs[index] = "i"
-  }
-
-  updateresultOnclick(index: number) {
-    this.showSearchs[index] = 'i'
-  }
-
 
   addGroup() {
     CamundaRest.createTaskGroupByID(
