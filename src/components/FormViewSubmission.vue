e8e9180219ae6814b5254b318624c43163689e00
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-03-31 21:28:14 +0530

    update css of FormView submission

--- src/components/FormViewSubmission.vue
+++ src/components/FormViewSubmission.vue
@@ -6,6 +6,9 @@
 </template>
 
 <script lang="ts">
+import "font-awesome/scss/font-awesome.scss";
+import 'semantic-ui-css/semantic.min.css';
+import '../user-styles.css'
 import { Component, Prop,  Vue } from 'vue-property-decorator'
 import { Form, Formio } from 'vue-formio';
 import { getFormDetails } from "../services/get-formio";
