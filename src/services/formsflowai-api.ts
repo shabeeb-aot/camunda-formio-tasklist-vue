f2c4f35d675cb206d85a3aa7f388e96a9f395481
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-04-06 18:37:55 +0530

    update intendation

--- src/services/formsflowai-api.ts
+++ src/services/formsflowai-api.ts
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 
 export const httpPOSTRequest = (url: string, data: any, token: string, isBearer = true) => {
-    return axios.post(url, data, {
-      headers: {
-        Authorization: isBearer
-          ? `Bearer ${token}`
-          : token,
-      },
-    });
-  };
+  return axios.post(url, data, {
+    headers: {
+      Authorization: isBearer
+        ? `Bearer ${token}`
+        : token,
+    },
+  });
+};
 
 export const formApplicationSubmit = (Apiurl: string, data: any, token: string) => {
-    return httpPOSTRequest(Apiurl + "/application/create", data, token)
+  return httpPOSTRequest(Apiurl + "/application/create", data, token)
 }
